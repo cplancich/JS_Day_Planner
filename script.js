386a26ba038fafa5aa196ba902839d9a464f3721
colinplancich <colin.plancich@gmail.com>
2022-06-30 19:27:05 -0700

    Added timeblock elements to html and coded class stylings for time blocks based on current moment

--- script.js
+++ script.js
@@ -3,48 +3,45 @@ var currentTime = moment();
 // DECLARE Current Hour
 var currentHour = moment().format('HH');
 // DECLARE Current Day HTML Element
-
+var currentDayEl = $('#currentDay').text(moment().format('dddd, MMMM, do, YYYY'));
 // DECLARE Time Entries Container HTML Element
 
-// Render a block for each hour of the day
-for(var i=9; i<=17; i++) {
+function timeBlockStyling() {
+
+var formInput = $(".description");
+
+for (let i = 0; i < formInput.length; i++) {
     
-    var key = "hour-"+i;
-    // Attempt to get the saved data for the hour of the loop
-    var data = "asdf";
-
-    // Compare i to current hour to determine if this hour is in the past, present, or future
-
-    var template =`
-        <div class="row">
-            <div>
-                ${i}AM
-            </div>
-            <div id="time-entry">
-                <textarea>${data}</textarea>
-            </div>
-            <div>
-                <button data-hour="${i}"Save</button>
-            </div>
-        </div>
-    `;
-
-    // Append the html to the page
+    var hourEl = formInput[i].dataset.hour
+    console.log(hourEl);
+    
+   if (currentHour === hourEl) {
+       formInput[i].classList.add("present");
+   } else if (currentHour > hourEl) {
+        formInput[i].classList.add("past");
+   } else {
+        formInput[i].classList.add("future");
+   }
+}
 
 }
 
+timeBlockStyling();
+
 // Set up a click event listener on the container
-timeEntryEl.on("click", "button", function(event) {
+// timeEntryEl.on("click", "button", function(event) {
 
-    // Fetch the hour from the clicked button's (event.target) "data-hour" attribute
+//     // Fetch the hour from the clicked button's (event.target) "data-hour" attribute (data-hour moved to text area from button)
 
-    // Use the hour to create the key for local storage
+//     // Use the hour to create the key for local storage
 
-    // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
+//     // From the clicked button, traverse the DOM to the nearby <textarea> to fetch its value
 
-    // Use the key and the value to save into localStorage
+//     // Use the key and the value to save into localStorage
 
-});
+// });
 
-
+// localStorage.setItem(KEY, JSON.stringify (user text value))
+// var localstoragedatalol = JSON.parse(localStorage.getItem("KEY"))
+// on page load, show previous data that was saved
     // Save an hour to local storage
